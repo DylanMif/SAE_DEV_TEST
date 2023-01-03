eeff51c30e8f85a4b2527ba3ae0534db45216406
DylanMif <Dylan.Miftari@etu.univ-savoie.fr>
2023-01-03 10:40:29 +0100

    Titre fenetre

--- JeuTest/JeuTest/Game1.cs
+++ JeuTest/JeuTest/Game1.cs
@@ -19,7 +19,7 @@ namespace JeuTest
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            Window.Title = "Test";
             base.Initialize();
         }
 
